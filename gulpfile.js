f9358bf9b40c76199c7b9395cbf87e6cd11af8f8
Alexander <alexmal74@gmail.com>
2022-09-10 00:43:20 +0500

    Набросал gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,130 @@
+'use strict';
+
+const { src, dest, parallel, series, watch } = require('gulp');
+const del = require('del');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
+const autoprefixer = require('gulp-autoprefixer');
+const gcssmq = require('gulp-group-css-media-queries');
+const includeFiles = require('gulp-include');
+const browserSync = require('browser-sync').create();
+
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: './public/',
+      serveStaticOptions: {
+        extensions: ['html'],
+      },
+    },
+    port: 8080,
+    ui: { port: 8081 },
+    open: true,
+  });
+}
+
+function views() {
+  return src('./src/*.pug')
+    .pipe(
+      pug({
+        pretty: true
+      })
+    )
+    .pipe(dest('./dist'));
+};
+
+function styles() {
+  return src('./src/styles/style.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({ grid: true }))
+  .pipe(gcssmq())
+  .pipe(dest('./public/css/'))
+  .pipe(browserSync.stream());
+}
+
+
+function scripts() {
+  return src('./src/js/script.js')
+  .pipe(
+    includeFiles({
+      includePaths: './src/components/**/',
+    })
+  )
+  .pipe(dest('./public/js/'))
+  .pipe(browserSync.stream());
+}
+
+
+function pages() {
+  return src('./src/pages/*.html')
+  .pipe(
+    includeFiles({
+      includePaths: './src/components/**/',
+    })
+  )
+  .pipe(dest('./public/'))
+  .pipe(browserSync.reload({ stream: true, }));
+}
+
+
+function copyFonts() {
+  return src('./src/fonts/**/*')
+  .pipe(dest('./public/fonts/'));
+}
+
+function copyImages() {
+  return src('./src/images/**/*')
+  .pipe(dest('./public/images/'));
+}
+
+async function copyResources() {
+  copyFonts();
+  copyImages();
+}
+
+async function clean() {
+  return del.sync('./public/', { force: true });
+}
+
+
+function watchDev() {
+  watch(['./src/js/script.js', './src/components/**/*.js'], scripts);
+  watch(['./src/styles/style.scss', './src/components/**/*.scss'], styles).on(
+    'change',
+    browserSync.reload
+  );
+  watch(['./src/pages/*.html', './src/components/**/*.html'], pages).on(
+    'change',
+    browserSync.reload
+  );
+}
+
+
+exports.browsersync = browsersync;
+exports.views = views;
+exports.clean = clean;
+exports.scripts = scripts;
+exports.styles = styles;
+exports.pages = pages;
+exports.copyResources = copyResources;
+
+exports.default = parallel(
+  clean,
+  views,
+  styles,
+  scripts,
+  copyResources,
+  pages,
+  browsersync,
+  watchDev
+);
+
+exports.build = series(
+  clean,
+  views,
+  styles,
+  scripts,
+  copyResources,
+  pages
+);
