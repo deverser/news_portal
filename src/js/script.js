da2eae9b556e204d29f5d55083982ec50a8c0f10
Alexander <alexmal74@gmail.com>
2022-09-18 05:32:54 +0500

    Добавлены стили для неактивных опций селектора рубрик

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
 'use strict';
 
-const select1 = new ItcCustomSelect('#select-1');
+//const select1 = new ItcCustomSelect('#select-1');
+
+let elem = document.getElementById('select-1');
+console.log(elem);
